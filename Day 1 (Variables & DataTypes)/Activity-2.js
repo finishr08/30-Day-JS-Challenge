1a8182104f00e4214071edcd9ba2c69fbd70ccfa
Mustafa Ahmed <contactmustafa8@gmail.com>
2024-07-13 16:50:49 +0500

    Activity 2

--- Day 1 (Variables & DataTypes)/Activity-2.js
+++ Day 1 (Variables & DataTypes)/Activity-2.js
@@ -3,6 +3,6 @@ Activity 2: Constant Declaration
 Task 3: Declare a variable using const, assign it a boolean value, and log the value to the console.
 */
 
-const booleanValue = true;
+const booleanValue = false;
 
 console.log(booleanValue);

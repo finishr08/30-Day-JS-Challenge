00d5316af1206541a6228c276ff1e0ac8aabcd28
Mustafa Ahmed <contactmustafa8@gmail.com>
2024-07-13 16:17:48 +0500

    Task-1

---
+++ Day 1 (Variables & DataTypes)/Tasks-1.js
@@ -0,0 +1,10 @@
+/*Tasks/Activities:
+
+Activity 1: Variable Declaration
+Task 1: Declare a variable using var, assign it a number, and log the value to the console.
+Task 2: Declare a variable using let, assign it a string, and log the value to the console.*/
+
+var number = 78;
+let string = "Mustafa";
+
+console.log(number, string);

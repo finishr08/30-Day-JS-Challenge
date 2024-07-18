9081ef285163d1da066792202d18618bac80847b
Mustafa Ahmed <contactmustafa8@gmail.com>
2024-07-18 11:30:12 +0500

    Activity-5

---
+++ Day 6 (Arrays)/Activity-5.js
@@ -0,0 +1,21 @@
+/*
+Activity 5: Multi-dimensional Arrays
+Task 12: Create a two-dimensional array (matrix) and log the entire array to the console.
+Task 13: Access and log a specific element from the two-dimensional array.
+*/
+
+// Task-12
+
+const matrix = [
+    [1, 2, 3],
+    [4, 5, 6],
+    [7, 8, 9]
+];
+  
+console.log(matrix);
+  
+
+// Task-13
+
+const specificElement = matrix[1][2];
+console.log(specificElement);

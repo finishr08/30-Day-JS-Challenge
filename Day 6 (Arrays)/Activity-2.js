ae509001c073df78db5ee5d545b3e292c0eff98e
Mustafa Ahmed <contactmustafa8@gmail.com>
2024-07-18 11:15:21 +0500

    Activity-2

---
+++ Day 6 (Arrays)/Activity-2.js
@@ -0,0 +1,31 @@
+/*
+Activity 2: Array Methods (Basic)
+Task 3: Use the push method to add a new number to the end of the array and log the updated array. 
+Task 4: Use the pop method to remove the last element from the array and log the updated array.
+Task 5: Use the shift method to remove the first element from the array and log the updated array.
+Task 6: Use the unshift method to add a new number to the beginning of the array and log the updated array.
+*/
+
+// Task-3
+
+let arrayList0 = [1,2,3];
+arrayList0.push(4);
+console.log(arrayList0);
+
+// Task-4
+
+let arrayList1 = [1,2,3];
+arrayList1.pop();
+console.log(arrayList1);
+
+// Task-5
+
+let arrayList2 = [1,2,3];
+arrayList2.shift();
+console.log(arrayList2);
+
+// Task-6
+
+let arrayList3 = [1,2,3];
+arrayList3.unshift(0);
+console.log(arrayList3);

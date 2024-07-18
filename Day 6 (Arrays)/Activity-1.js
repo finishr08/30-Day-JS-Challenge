8304eac41cfb406efe81815728bcdcd5f0ed6a89
Mustafa Ahmed <contactmustafa8@gmail.com>
2024-07-18 11:09:34 +0500

    Activity-1

---
+++ Day 6 (Arrays)/Activity-1.js
@@ -0,0 +1,15 @@
+/*
+Activity 1: Array Creation and Access
+Task 1: Create an array of numbers from 1 to 5 and log the array to the console.
+Task 2: Access the first and last elements of the array and log them to the console.
+*/
+
+// Task-1
+
+let arrayList = [1,2,3,4,5];
+console.log(arrayList);
+
+// Task-2
+
+let arrayList1 = [1,2,3,4,5];
+console.log(arrayList1[0], arrayList1[4]);

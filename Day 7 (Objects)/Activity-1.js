61c78e24bcb254b3caa23c2ecd2638c3d5c681f9
Mustafa Ahmed <contactmustafa8@gmail.com>
2024-07-19 11:55:34 +0500

    Activity-1

---
+++ Day 7 (Objects)/Activity-1.js
@@ -0,0 +1,21 @@
+/*
+Activity 1: Object Creation and Access
+Task 1: Create an object representing a book with properties like title, author, and year, and log the object to the console.
+Task 2: Access and log the title and author properties of the book object.
+*/
+
+// Task-1
+
+const bookDetail = {
+    title: "Nextjs",
+    author: "Mustafa",
+    year: 2023
+}
+
+console.log(bookDetail);
+
+
+// task-2
+
+let access = [bookDetail.title, bookDetail.author]
+console.log(access);

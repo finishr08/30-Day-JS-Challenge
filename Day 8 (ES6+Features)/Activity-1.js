5d6791352671342a2b883303ea0355092e361ca3
Mustafa Ahmed <contactmustafa8@gmail.com>
2024-07-20 09:37:41 +0500

    Activity-1

---
+++ Day 8 (ES6+Features)/Activity-1.js
@@ -0,0 +1,22 @@
+/*
+Activity 1: Template Literals
+Task 1: Use template literals to create a string that includes variables for a person's name and age, and log the string to the console.
+Task 2: Create a multi-line string using template literals and log it to the console.
+*/
+
+// Task-1
+
+const name = "Mustafa";
+const age = 20;
+
+const message = `AOA, my name is ${name} and I am ${age} years old.`;
+console.log(message);
+
+
+// Task-2
+
+const stringValue = `
+Hlo my name is Mustafa
+my age is 20 years old`
+
+console.log(stringValue);

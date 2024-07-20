b2524b95cdacaa4a1f946ce4987813f8de622df7
Mustafa Ahmed <contactmustafa8@gmail.com>
2024-07-20 09:53:35 +0500

    Activity-4

--- Day 8 (ES6+Features)/Activity-4.js
+++ Day 8 (ES6+Features)/Activity-4.js
@@ -2,3 +2,14 @@
 Activity 4: Default Parameters
 Task 7: Write a function that takes two parameters and returns their product, with the second parameter having a default value of 1. Log the result of calling this function with and without the second parameter.
 */
+
+function multiply(a, b = 1) {
+    return a * b;
+  }
+  
+  // Calling the function with both parameters
+  console.log(multiply(5, 3)); 
+  
+  // Calling the function with only the first parameter
+  console.log(multiply(7)); 
+  

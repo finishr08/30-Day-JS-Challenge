2f144efdf63fe4c4472edc90a7cbd9472785fdf7
Mustafa Ahmed <contactmustafa8@gmail.com>
2024-07-25 10:53:18 +0500

    Day 13 (Modules)

---
+++ JS Challenge Day (11 to 20)/Day 13 (Modules)/Activity-5/src/index.js
@@ -0,0 +1,5 @@
+// src/index.js
+import { add, subtract } from './math';
+
+console.log(`10 + 5 = ${add(10, 5)}`);
+console.log(`10 - 5 = ${subtract(10, 5)}`);
---
+++ JS Challenge Day (11 to 20)/Day 13 (Modules)/Activity-5/src/math.js
@@ -0,0 +1,9 @@
+// src/math.js
+export function add(a, b) {
+    return a + b;
+  }
+  
+  export function subtract(a, b) {
+    return a - b;
+  }
+  
